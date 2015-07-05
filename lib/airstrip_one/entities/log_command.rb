a8f300e7fa5f994fb19f3e694b691a9393d35434
Bernat Rafales <brafales@gmail.com>
2015-07-05 12:17:20 +0100

    Added option to search log

--- lib/airstrip_one/entities/log_command.rb
+++ lib/airstrip_one/entities/log_command.rb
@@ -1,9 +1,24 @@
 class LogCommand
   def self.process(message:, chat_id:)
-    log_id = message.match(/log (\d+)/)[1].to_i
-    if log_id > 0
-      pinchito_log = Pinchito::Log.from_id(log_id)
-      TelegramApi::Logger.error(TelegramBotApi::Client.send_message(pinchito_log.to_s, chat_id))
+    if log_id = message.match(/\/log (\d+)/)
+      message = self.by_log_id(log_id[1].to_i)
+    elsif query_string = message.match(/\/search ([\w\s\.,-\?!]+)/)
+      message = self.by_query_string(query_string[1])
     end
+    if message
+      TelegramApi::Logger.error(TelegramBotApi::Client.send_message(message, chat_id))
+    end
+  end
+
+  private
+
+  def self.by_log_id(log_id)
+    if log_id > 0
+      Pinchito::Log.from_id(log_id)
+    end
+  end
+
+  def self.by_query_string(query_string)
+    Pinchito::Log.from_search(query_string)
   end
 end
